9191b9b9796b6d1c6186acf8cc833b46e793bea2
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-09-29 13:08:32 +0530

    Add files via upload

---
+++ hangman unfinished.py
@@ -0,0 +1,41 @@
+import random
+HANGMAN_PICS= ['''
+    +---+
+        |
+        |
+        |
+       ===''','''
+    +---+
+    O   |   
+        |
+        |
+       ===''','''
+    +---+
+    O   |   
+    |   |
+        |
+       ===''','''
+    +---+
+    O   |   
+   /|   |
+        |
+       ===''','''
+    +---+
+    O   |   
+   /|\  |
+        |
+       ===''',''' 
+    +---+
+    O   |   
+   /|\  |
+   /    |
+       ===''','''
+    +---+
+    O   |   
+   /|\  |
+   / \  |
+       ===''']                   
+words= "ant babbon badger bat beaver camer cat clam cobra cougar coyote crow deer
+       
+       
+       
